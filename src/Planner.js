651d3f386acd80565cfc985ec148e926e17dfe3c
incredulous-chordate <incredulouschordate@gmail.com>
2023-06-15 15:52:15 -0400

    fix value bug, and round results nicer

--- src/Planner.js
+++ src/Planner.js
@@ -18,31 +18,31 @@ const Planner = ({
         <div className='Inputs'>
             <div>
                 <p>Block height: </p>
-                <input placeholder='block height' text={height} onChange={event => setHeight(event.target.value)} value={"40"} />
+                <input placeholder='block height' text={height} onChange={event => setHeight(event.target.value)} defaultValue={"40"} />
             </div>
             <div>
                 <p>Block width: </p>
-                <input placeholder='block width' text={width} onChange={event => setWidth(event.target.value)} value={"80"} />
+                <input placeholder='block width' text={width} onChange={event => setWidth(event.target.value)} defaultValue={"80"} />
             </div>
             <div>
                 <p>Street width: </p>
-                <input placeholder='street width' text={streetWidth} onChange={event => setStreetWidth(event.target.value)} value={"50"} />
+                <input placeholder='street width' text={streetWidth} onChange={event => setStreetWidth(event.target.value)} defaultValue={"50"} />
             </div>
             <div>
                 <p>House frontage: </p>
-                <input placeholder='house frontage' text={frontage} onChange={event => setFrontage(event.target.value)} value={"8"} />
+                <input placeholder='house frontage' text={frontage} onChange={event => setFrontage(event.target.value)} defaultValue={"8"} />
             </div>
             <div>
                 <p>People per house: </p>
-                <input placeholder='people per house' text={peoplePerHouse} onChange={event => setPeoplePerHouse(event.target.value)} value={"2.2"} />
+                <input placeholder='people per house' text={peoplePerHouse} onChange={event => setPeoplePerHouse(event.target.value)} defaultValue={"2.2"} />
             </div>
             <div>
                 <p>Portion of total buildable lots used by housing: </p>
-                <input placeholder='Residential portion' text={residentialArea} onChange={event => setResidentialArea(event.target.value)} value={".9"} />
+                <input placeholder='Residential portion' text={residentialArea} onChange={event => setResidentialArea(event.target.value)} defaultValue={".9"} />
             </div>
             <div>
                 <p>Portion of total neighborhood area used by parks: </p>
-                <input placeholder='Park portion' text={parkArea} onChange={event => setParkArea(event.target.value)} value={".1"} />
+                <input placeholder='Park portion' text={parkArea} onChange={event => setParkArea(event.target.value)} defaultValue={".1"} />
             </div>
             
         </div>
@@ -79,11 +79,13 @@ const Stats = ({
     const densityKilos = peoplePerBlock/cityAreaPerBlock * residentialArea * 1000000
     
     return <div className='Stats'>
-        <p>Population density: {densityKilos} people/km^2</p>
-        <p>{densityKilos * 2.58998811} people/mile^2</p>
-        <p>{densityKilos * 0.00404686} people/acre</p>
+        <p>Population density: {roundTo2(densityKilos)} people/km^2</p>
+        <p>{roundTo2(densityKilos* 2.58998811) } people/mile^2</p>
+        <p>{roundTo2(densityKilos* 0.00404686) } people/acre</p>
         <p>Average lot size: {frontage * height/2}</p>
     </div>
 }
+
+const roundTo2 = num => Math.round(num * 100) / 100
 
 export default Planner
